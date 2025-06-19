1ff8f988131b78804123bca74b8707bb4b6141ee
Имя ФÐТNikita Lar'лÑлÑkin <larkinnikita12@gmail.com>
2025-06-20 01:04:42 +0300

    Добавлены НЕ ПОЛНОСТЬЮ ГОТОВЫЕ модели: Recipe.

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -11,6 +11,7 @@ UNIT_MAX_LENGTH = 20
 class Tag(models.Model):
     title = models.CharField(unique=True, blank=False, null=False,
                              max_length=TITLE_MAX_LENGTH, verbose_name='Название тега')
+
     slug = models.SlugField(unique=True, blank=False, null=False,
                             max_length=SLUG_MAX_LENGTH, verbose_name='Идентификатор')
 
@@ -21,8 +22,25 @@ class Tag(models.Model):
 class Ingredient(models.Model):
     title = models.CharField(blank=False, null=False,
                              max_length=TITLE_MAX_LENGTH, verbose_name='Название ингредиента')
+
     unit = models.CharField(blank=False, null=False,
                             max_length=UNIT_MAX_LENGTH, verbose_name='Название единицы измерения')
 
     def __str__(self):
         return self.title
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               blank=False, null=False,
+                               verbose_name='Автор рецепта', related_name='recipes')
+
+    title = models.CharField(blank=False, null=False,
+                             max_length=TITLE_MAX_LENGTH, verbose_name='Название рецепта')
+
+    image = models.ImageField('Картинка', upload_to='recipe_pictures', blank=False)
+
+    description = models.TextField(verbose_name='Описание рецепта')
+
+    cooking_time = models.PositiveIntegerField(blank=False, null=False,
+                                               verbose_name='Время приготовления в мин.')
