3f348d289ad4b522a7561d58b2d9701671127de8
Имя ФÐТNikita Lar'лÑлÑkin <larkinnikita12@gmail.com>
2025-06-20 04:50:27 +0300

    Полностью готова модель Recipe.

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -31,16 +31,36 @@ class Ingredient(models.Model):
 
 
 class Recipe(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               blank=False, null=False,
-                               verbose_name='Автор рецепта', related_name='recipes')
+    author = models.ForeignKey(User, blank=False, null=False,
+                               on_delete=models.CASCADE, related_name='recipes',
+                               verbose_name='Автор рецепта', )
 
     title = models.CharField(blank=False, null=False,
                              max_length=TITLE_MAX_LENGTH, verbose_name='Название рецепта')
 
-    image = models.ImageField('Картинка', upload_to='recipe_pictures', blank=False)
+    image = models.ImageField(blank=False, null=False,
+                              verbose_name='Картинка', upload_to='recipe_pictures')
 
-    description = models.TextField(verbose_name='Описание рецепта')
+    description = models.TextField(blank=False, null=False, verbose_name='Описание рецепта')
+
+    ingredients = models.ManyToManyField(Ingredient, blank=False, through='RecipeIngredient',
+                                         related_name='recipes', verbose_name='Ингредиенты')
+
+    tags = models.ManyToManyField(Tag, blank=False, related_name='recipes', verbose_name='Теги')
 
     cooking_time = models.PositiveIntegerField(blank=False, null=False,
                                                verbose_name='Время приготовления в мин.')
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='recipe_ingredients')
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='ingredient_recipes')
+    quantity = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Количество')
+    unit = models.CharField(max_length=UNIT_MAX_LENGTH, verbose_name='Название единицы измерения')
+
+    class Meta:
+        verbose_name = 'Ингредиент рецепта'
+        verbose_name_plural = 'Ингредиенты рецепта'
+
+    def __str__(self):
+        return f'{self.quantity} {self.unit} {self.ingredient.title}'
