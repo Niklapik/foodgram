0dc70fef9795d644e2554d020c9cbaa71cadc371
Имя ФÐТNikita Lar'лÑлÑkin <larkinnikita12@gmail.com>
2025-08-19 21:25:19 +0300

    Добавлена правильная настройка отображения картинок

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = '/backend_static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
